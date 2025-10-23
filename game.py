1f2649fba679c115142bb573d9e1d50b8603a1ae
codingmotu <codingmotu@gmail.com>
2025-10-23 14:38:49 -0300

    chore: commit test 56

--- game.py
+++ game.py
@@ -57,7 +57,7 @@ def jogar():
         tabuleiro = [str(i) for i in range(1, 10)]
         jogador = "X"
         limpar_tela()
-        printse("=== Jogo da Velha ===")
+        printer("=== Jogo da Velha ===")
         print("Digite 'q' para sair a qualquer momento.")
         desenhar_tabuleiro(tabuleiro)
 
